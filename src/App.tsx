d09933073eaa1674ac4e16d2679228e1430db0ba
koo <hyunmo0904@gmail.com>
2021-11-10 12:41:16 +0900

    feat:반응형 구조

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import React from "react";
+import { useMediaQuery } from "react-responsive";
+import { Desktop as DesktopMain } from "./desktop/Main";
+import { Mobile as MobileMain } from "./mobile/Main";
 
-function App() {
-  return (
-    <div className="App">
-      <p>asd</p>
-    </div>
-  );
-}
+const Desktop = () => {
+  const isDesktop = useMediaQuery({ minWidth: 768 });
+  return isDesktop ? <DesktopMain /> : null;
+};
+
+const Mobile = () => {
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+  return isMobile ? <MobileMain /> : null;
+};
+
+const App = () => (
+  <div>
+    <Desktop />
+    <Mobile />
+  </div>
+);
 
 export default App;
---
+++ src/mobile/Main.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function Mobile() {
+  return <p>Mobile</p>;
+}
